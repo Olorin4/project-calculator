2a1965d61ab2f7a6e4bf2cccf04ff4a8b5503b60
Nick Kalas <nick.kalas@proton.me>
2024-04-06 12:05:54 +0300

    Add functions for subtraction, multiplication and division

--- javascript.js
+++ javascript.js
@@ -1,7 +1,29 @@
-// Addition start
-
+// Addition
 function addition(arr) {
     return arr.reduce((accumulator, current) => accumulator + current , 0);
 }
 
-let result = addition([1, 2, 3, 4, 5]);
+addition([1, 2, 3, 4, 5]);
+
+
+// Subtraction
+function subtraction(a, b) {
+    return a - b;
+}
+
+subtraction(55, 24);
+
+
+// Multiply
+function muliplication(a, b) {
+    return a * b;
+}
+
+muliplication(5, 8);
+
+//Division
+function division(a, b) {
+    return a / b;
+}
+
+division(63, 9);
