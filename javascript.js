a207bbdbc5e999386fce7569d4afb07d60f4f050
Nick Kalas <nick.kalas@proton.me>
2024-04-06 12:36:38 +0300

    Add function for calling the operation functions. Add function for remainder. Add a second paragraph for displaying user input

--- javascript.js
+++ javascript.js
@@ -2,42 +2,37 @@
 // function addition(arr) {
 //     return arr.reduce((accumulator, current) => accumulator + current , 0);
 // }
-addition(a, b) => a + b;
+function addition(a, b) {
+    return a + b;
+}
 
-
-addition(17, 32);
-
-
-// Subtraction
 function subtraction(a, b) {
     return a - b;
 }
 
-subtraction(55, 24);
-
-
-// Multiply
 function multiplication(a, b) {
     return a * b;
 }
 
-multiplication(5, 8);
-
-
-//Division
 function division(a, b) {
     return a / b;
 }
 
-division(63, 9);
-
+function remainder(a, b) {
+    return a % b;
+}
 
 let firstNum;
 let operator;
 let secondNum;
 
 function operate(firstNum, operator, secondNum) {
-    operate === '+' ? addition(firstNum, secondNum);
+    operator === '+' ? addition(firstNum, secondNum):
+    operator === '-' ? subtraction(firstNum, secondNum):
+    operator === '*' ? multiplication(firstNum, secondNum):
+    operator === '/' ? division(firstNum, secondNum):
+    operator === '%' ? remainder(firstNum, secondNum):
+    '';
 }
 
 
