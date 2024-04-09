5aff13f8a91fe016ab7243e66468da97ef83e2ee
Nick Kalas <nick.kalas@proton.me>
2024-04-10 00:29:20 +0300

    Partially fix string operations. More work to be done to fix the display update, multiplication and division

--- javascript.js
+++ javascript.js
@@ -1,6 +1,9 @@
+let Num1;
+let Num2;
+let currentOperator;
 let result;
 
-function operate(a, operator, b) {
+function compute(a, operator, b) {
     switch (operator) {
         case "+":
             result = a + b;
@@ -20,12 +23,8 @@ function operate(a, operator, b) {
         default:
             return
     }
+    output.textContent = result.toLocaleString();
 }
-
-
-let Num1;
-let currentOperator;
-let Num2;
 
 
 //Add functionality for number buttons
@@ -33,7 +32,7 @@ let output = document.querySelector(".output");
 
 function numberBtn(number) {
     if (number === "." && output.textContent.includes(".")) return
-    if (+output.textContent === 0 || result !== undefined) { 
+    if (+output.textContent === 0 || +output.textContent === result) { 
         output.textContent = number;
     } else {
         output.textContent += number;
@@ -49,38 +48,69 @@ document.querySelectorAll(".numbers").forEach(button => {
 
 //Add functionality for operator buttons
 let input = document.querySelector(".input");
-
-function operatorBtn(operator) {
-    if (+output.textContent === 0) return
-    if (Num1 === undefined && currentOperator === undefined) {
-        currentOperator = operator;
-        Num1 = +output.textContent;
-        input.textContent = `${output.textContent} ${operator} `;
-        output.textContent = "0"; 
-    } else if (Num2 === undefined) {
-        currentOperator = operator;
-        equalsBtn();
-    }
-}
+let isAssigningToOperator1 = true; // Flag variable to alternate assignments between the two operators
+let operator1;
+let operator2;
 
 document.querySelectorAll(".operators").forEach(button => {
     button.addEventListener("click", () => {
+        (isAssigningToOperator1) ?
+            (operator1 = button.innerText) :              // Alternate with each click
+            (operator2 = button.innerText) ;
+        isAssigningToOperator1 = !isAssigningToOperator1; // Toggle the flag variable for the next click
+        console.log(`operator1 = ${operator1}`);
+        console.log(`operator2 = ${operator2}`);
         operatorBtn(button.innerText);
     });
 });
 
+function operatorBtn(operator) {
+    if (+output.textContent === 0) return
+    console.log(`Num1 = ${Num1}`);
+    console.log(`Num2 = ${Num2}`);
+    if (Num1 === undefined && result === undefined) {
+        currentOperator = operator;
+        console.log(`currentOperator = ${currentOperator}`);
+        Num1 = +output.textContent;
+        input.textContent = `${output.textContent} ${operator} `;
+        output.textContent = "0"; 
+    } else if (Num1 === undefined && result !== undefined) {
+        if (Num1 === undefined) {
+            Num1 = result;
+            Num2 = +output.textContent;
+            console.log(`Num1 = ${Num1}`);
+            console.log(`Num2 = ${Num2}`);
+            if (operator1 !== operator) {
+                compute(Num1, operator1, Num2);
+                input.textContent = `${result} ${operator1} `;
+                output.textContent = result;
+            } else {
+                compute(Num1, operator2, Num2);
+                input.textContent = `${result} ${operator2} `;
+                output.textContent = result;
+            }
+            divideByZero()
+            undefineMainVariables();
+        }
+    } else {
+        equalsBtn();
+    }
+}
+
+
 //Add functionality for the '=' button
 function equalsBtn() {
-    if (+output.textContent === 0) return
+    if (+output.textContent === 0) return         // Prevents function from running 
     Num2 = +output.textContent;
+    console.log(`Num2 = ${Num2}`);
     input.textContent += `${output.textContent} =`;
-    operate(Num1, currentOperator, Num2);
-    output.textContent = result.toLocaleString();
-    Num1 = undefined;
-    Num2 = "";
-    currentOperator = undefined;
-    result = undefined;
+    if (operator1 !== undefined) {
+        compute(Num1, operator1, Num2);
+    } else {
+        compute(Num1, operator2, Num2);
+    }
     divideByZero()
+    undefineMainVariables();
 }
 
 document.querySelector("#equals").addEventListener("click", equalsBtn);
@@ -90,9 +120,9 @@ document.querySelector("#equals").addEventListener("click", equalsBtn);
 function clearBtn() {
     output.textContent = 0;
     input.textContent = " ";
-    Num1 = undefined;
-    Num2 = undefined;
-    currentOperator = undefined;
+    undefineMainVariables()
+    operator1 = undefined;
+    operator2 = undefined;
     result = undefined;
 }
 
@@ -118,7 +148,7 @@ document.querySelector("#backspace").addEventListener("click", () => {
 
 // Display an error message if the user tries to divide by 0
 function divideByZero() {
-    if (currentOperator === "/" && Num2 === 0) {
+    if (operator1 === "/" && Num2 === 0) {
         alert("ERROR! Don't you know you can't divide by 0? Pff...");
         clearBtn();
     }
@@ -136,6 +166,16 @@ function signBtn() {
 document.querySelector("#sign").addEventListener("click", () => {
     signBtn();
 });
+
+
+function undefineMainVariables() {
+    Num1 = undefined;
+    Num2 = undefined;
+    currentOperator = undefined;
+    // operator1 = undefined;
+    // operator2 = undefined;
+    // result = undefined;
+}
 
 
 
